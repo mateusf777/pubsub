fce5f9b1df08601d67b664b95d2fd39a7b7d80fe
Mateus Fernandes <mateus.fernandes@pearll.com>
2021-09-17 21:23:34 -0400

    Remove line from build.sh
    - example/cli is not there anymore

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ go build -o ./build/ps-server .  &&
   go build -o ./build/queue ./example/queue &&
   go build -o ./build/subscriber ./example/subscriber &&
   go build -o ./build/publisher ./example/publisher &&
-  go build -o ./build/request ./example/request &&
-  go build -o ./build/cli ./example/cli
+  go build -o ./build/request ./example/request
