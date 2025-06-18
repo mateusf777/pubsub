052b0036506514758179b591cd6f0f64a23f10a6
Mateus Fernandes <mateus.fernandes06@gmail.com>
2025-06-17 21:15:46 -0400

    Fix tests for windows

--- test.bat
+++ test.bat
@@ -1,52 +1,48 @@
 @echo off
 
-cd server &&
-go build -o ../build/ps-server ./cmd/pubsub
-cd ../example &&
-go build -o ../build/queue ./queue &&
-go build -o ../build/subscriber ./subscriber &&
-go build -o ../build/publisher ./publisher &&
-go build -o ../build/request ./request
+cd server
+go build -o ../build/ps-server.exe ./cmd/pubsub
+cd ../example
+go build -o ../build/queue.exe ./queue
+go build -o ../build/subscriber.exe ./subscriber
+go build -o ../build/publisher.exe ./publisher
+go build -o ../build/request.exe ./request
 
 cd ..
 
-docker build -f server/Dockerfile -t pubsub:latest .
-
-cd core &&
-mockery &&
+cd core
+mockery
 go clean -testcache
 go test . -race
 
 cd ..
 
-cd server &&
-mockery &&
+cd server
+mockery
 go clean -testcache
 go test . -race
 
 cd ..
 
-cd client &&
-mockery &&
+cd client
+mockery
 go clean -testcache
 go test . -race
 
 cd ..
 
-docker run -d --rm -p 9999:9999 --name test pubsub:latest
-docker run -d --rm ^
-    -e PUBSUB_TLS_CERT=/fullchain.pem ^
-    -e PUBSUB_TLS_KEY=/privkey.pem ^
-    -e PUBSUB_ADDRESS=0.0.0.0:9443 ^
-    -p 9443:9443 ^
-    -v %CERT_DIR%\fullchain.pem:/fullchain.pem ^
-    -v %CERT_DIR%\privkey.pem:/privkey.pem ^
-    --name test-tls pubsub:latest
+start "" /b build\ps-server.exe
+
+start "" /b cmd /c "set PUBSUB_TLS_CERT=%CERT_DIR%\fullchain.pem && set PUBSUB_TLS_KEY=%CERT_DIR%\privkey.pem && set PUBSUB_ADDRESS=0.0.0.0:9443 && build\ps-server.exe"
+
 
 cd example/integration
 go clean -testcache
-go test -v . -race
-docker stop test
-docker stop test-tls
+go test -v -race .
+
+REM Stop both servers
+for /f "tokens=2" %%a in ('tasklist /fi "imagename eq ps-server.exe" /fo list ^| findstr "PID"') do (
+    taskkill /PID %%a /F
+)
 
 cd ../..
