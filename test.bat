90cbbe6cf508de318d795b919dccaf68028aefbc
Mateus Fernandes <mateus.fernandes06@gmail.com>
2025-06-21 12:01:28 -0400

    Fix windows local ci

--- test.bat
+++ test.bat
@@ -31,25 +31,25 @@ go test . -race
 cd ..
 cd example/integration
 
-start "" /b build\ps-server.exe
+start "" /b ..\..\build\ps-server.exe
 
-go test -race -run '^(TestPublish|TestQueue|TestRequest)$' .
-
-for /f "tokens=2" %%a in ('tasklist /fi "imagename eq ps-server.exe" /fo list ^| findstr "PID"') do (
-    taskkill /PID %%a /F
-)
-
-start "" /b cmd /c "set PUBSUB_TLS_CERT=%CERT_DIR%\fullchain.pem && set PUBSUB_TLS_KEY=%CERT_DIR%\privkey.pem && set PUBSUB_ADDRESS=0.0.0.0:9443 && build\ps-server.exe"
-
-go test -race -run '^(TestPublishTLS|TestQueueTLS|TestRequestTLS)$' .
+go test -race -run "^(TestPublish|TestQueue|TestRequest)$" .
 
 for /f "tokens=2" %%a in ('tasklist /fi "imagename eq ps-server.exe" /fo list ^| findstr "PID"') do (
     taskkill /PID %%a /F
 )
 
-start "" /b cmd /c "set PUBSUB_TLS_CERT=%CERT_DIR%\fullchain.pem && set PUBSUB_TLS_KEY=%CERT_DIR%\privkey.pem && set PUBSUB_TLS_CA=%CERT_DIR%\myca-cert.pem && set PUBSUB_ADDRESS=0.0.0.0:9443 && build\ps-server.exe"
+start "" /b cmd /c "set PUBSUB_TLS_CERT=%CERT_DIR%\fullchain.pem && set PUBSUB_TLS_KEY=%CERT_DIR%\privkey.pem && set PUBSUB_ADDRESS=0.0.0.0:9443 && ..\..\build\ps-server.exe"
 
-go test -race -run '^(TestConnectTLSNoCert|TestConnectTLSInvalidCert|TestConnectTLS)$' .
+go test -race -run "^(TestPublishTLS|TestQueueTLS|TestRequestTLS)$" .
+
+for /f "tokens=2" %%a in ('tasklist /fi "imagename eq ps-server.exe" /fo list ^| findstr "PID"') do (
+    taskkill /PID %%a /F
+)
+
+start "" /b cmd /c "set PUBSUB_TLS_CERT=%CERT_DIR%\fullchain.pem && set PUBSUB_TLS_KEY=%CERT_DIR%\privkey.pem && set PUBSUB_TLS_CA=%CERT_DIR%\myca-cert.pem && set PUBSUB_ADDRESS=0.0.0.0:9443 && ..\..\build\ps-server.exe"
+
+go test -race -run "^(TestConnectTLSNoCert|TestConnectTLSInvalidCert|TestConnectTLS)$" .
 
 for /f "tokens=2" %%a in ('tasklist /fi "imagename eq ps-server.exe" /fo list ^| findstr "PID"') do (
     taskkill /PID %%a /F
